ea3194707d06955f8e34ccf6261a1fe4bbc4e49d
MOGAR <alicea,9a@gmail.com>
2024-06-25 09:53:52 -0600

    added event listeners for new buttons selections.

--- rock_js_script.js
+++ rock_js_script.js
@@ -1,67 +1,49 @@
-/* 
-A console version of rock paper scissors
-itll get inputs from the user
-the output should be the choice made by the user and by the computer..
-.. stating weather the user won or lost
-it will need a random option from the computer and the input from the..
-..user to work properly. it will also need a function to determine the winner and..
-display the results 
-*/ 
+let ROCK = document.querySelector('#rock');
+let PAPER = document.querySelector('#paper');
+let SCISSORS = document.querySelector('#scissors');
 
-let keepScore = 0; 
-// function that will make the computers random choice and switch case to convert it to strings
+let buttons =[ROCK,PAPER,SCISSORS];
+
+let addScore = 0; 
 
 
+// outputs a num between 1-3 and returns a corelating string
 function getComputerSelection(){
     let computerNum = Math.floor(Math.random() * 3) + 1;
     if(computerNum === 1){
-        return "ROCK"
+        return "ROCK";
     } else if(computerNum === 2){
-        return "PAPER"
+        return "PAPER";
     } else if(computerNum === 3){
-        return "SCISSORS"
+        return "SCISSORS";
     }
 }
-// function that plays a single round that uses the computer/user choices as parameters
+// runs a single round while keeping score. display current result of each game
 
 
 function playRound (computerChoice,playerChoice){
 
      if(playerChoice === computerChoice){
-        console.log(`Its a tie!`)
+        console.log(`Its a tie!`);
 
     } else if(playerChoice === "ROCK" && computerChoice === "SCISSORS"){
         console.log(`You win! ${playerChoice} beats ${computerChoice}!`);
-        keepScore++;
+        addScore++;
     } else if(playerChoice === "PAPER" && computerChoice ==="ROCK"){
         console.log(`You win! ${playerChoice} beats ${computerChoice}!`);
-        keepScore++;
+        addScore++;
     }else if(playerChoice === "SCISSORS" && computerChoice ==="PAPER"){
         console.log(`You win! ${playerChoice} beats ${computerChoice}!`);
-        keepScore++;
+        addScore++;
     } else{
         console.log(`You lost! ${computerChoice} beats ${playerChoice}`);
     }
     
-    return keepScore;
-}
-// function that uses and loops five rounds of rock paper scissor
-function playGame(){
-    for (i = 0; i < 5; i++){
+    return addScore;
+};
 
-        let playerChoice = prompt("Rock Paper or Scissors?").toUpperCase();
-        console.log(`users choice was ${playerChoice}`);
-
-        let computerChoice = getComputerSelection();
-        console.log(`comps choice was ${computerChoice}`);
-        
-        console.log(playRound(computerChoice,playerChoice));
-        
-    }
-    if( i === 5 && keepScore >= 3){
-        return console.log(`You won ${keepScore}/5 games! You're the winner!`)
-    }else if(i === 5 && keepScore < 3){
-        return console.log(`You won ${keepScore}/5 games! The computer is the winner!`)
-    }
-}
-playGame();
+buttons.forEach((button) =>{
+    button.addEventListener('click', ()=>{
+        alert('it works!');
+    })
+})
